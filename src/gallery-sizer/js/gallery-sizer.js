c8ba926bafcd2dd357a29188fe47e3b0b2d61c3c
Anthony Pipkin <a.pipkin@yahoo.com>
2012-11-16 08:44:17 -0800

    Update typo to fix example.

--- src/gallery-sizer/js/gallery-sizer.js
+++ src/gallery-sizer/js/gallery-sizer.js
@@ -6,7 +6,7 @@ Demonstrates the use of Resizer and image placeholder services.
 @class Sizer
 @since 0.1
 **/
-Y.Sizer = Y.Base.create('gallery-sizer', Y.Widget, [], {
+Y.Sizer = Y.Base.create('sizer', Y.Widget, [], {
 
     /**
     Creates image and text nodes and calls _makeResize
